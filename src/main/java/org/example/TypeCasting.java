f09684536b64728d2768d81490745adf9b8fba07
ImeshaDilshani <imeshadilshani212@gmail.com>
2023-12-17 20:26:41 +0530

    Type Casting

---
+++ src/main/java/org/example/TypeCasting.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class TypeCasting {
+    public static void main(String[] arg){
+        int intvalue = 100;
+        //implicit type casting from int to long
+        long longvalue = intvalue;
+        //implicit type casting from long to double
+        double douublevalue = longvalue;
+        System.out.println(intvalue);
+        System.out.println(longvalue);
+        System.out.println(douublevalue);
+    }
+}
