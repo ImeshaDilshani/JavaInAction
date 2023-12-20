4d7721e82d33423a27bc7b39d2242ce6b107a166
ImeshaDilshani <imeshadilshani212@gmail.com>
2023-12-20 09:37:49 +0530

    Parameterized Constructors

--- src/main/java/org/example/DefaultConstruactor.java
+++ src/main/java/org/example/DefaultConstruactor.java
@@ -13,19 +13,26 @@ class Animal{
         this.animalAge = 8;
     }
 
+    public Animal(int aID, String aName, int aAge){
+        this.animalID = aID;
+        this.animalName = aName;
+        this.animalAge = aAge;
+    }
+
     public void DisplayDetails(){
         System.out.println("Student ID : " + animalID);
         System.out.println("Student Name : " + animalName);
         System.out.println("Student Age : " + animalAge);
     }
 
-    public void AcceptDetails() {
-    }
 }
 public class DefaultConstruactor {
     public static void main(String[] arg){
         Animal animal = new Animal();
         animal.DisplayDetails();
 
+        Animal animal1 = new Animal(2,"cat",3);
+        animal1.DisplayDetails();
     }
+
 }
