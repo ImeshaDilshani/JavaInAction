0d9f101b122b626a35577dafd3c59f3c99b5bfa5
ImeshaDilshani <imeshadilshani212@gmail.com>
2023-12-20 14:55:33 +0530

    Implementing Inheritance between Classes

--- src/main/java/org/example/ClassAndObject.java
+++ src/main/java/org/example/ClassAndObject.java
@@ -2,74 +2,80 @@ package org.example;
 
 import java.util.Scanner;
 
-class Student{
+class Student {
     int studentID;
     String studentName;
     int studentAge;
     Scanner scanner = new Scanner(System.in);
 
-    public void AcceptDetails(){
-        System.out.println("Enter Student ID : ");
+    public void AcceptDetails() {
+        System.out.println("Enter Student ID: ");
         studentID = scanner.nextInt();
-        System.out.println("Enter Student Name : ");
+        System.out.println("Enter Student Name: ");
         studentName = scanner.next();
-        System.out.println("Enter Student Age : ");
+        System.out.println("Enter Student Age: ");
         studentAge = scanner.nextInt();
     }
 
-    public void DisplayDetails(){
-        System.out.println("Student ID : " + studentID);
-        System.out.println("Student Name : " + studentName);
-        System.out.println("Student Age : " + studentAge);
+    public void DisplayDetails() {
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Student Age: " + studentAge);
     }
-
 }
 
-class Marks extends Student{
-    private float objectiveMarks;
-    private float subjectiveMarks;
+class Marks extends Student {
+    protected float objectiveMarks;
+    protected float subjectiveMarks;
 
     Scanner scanner = new Scanner(System.in);
 
-    public void AcceptDetails(){
+    public void AcceptDetails() {
         super.AcceptDetails();
-        System.out.println("Enter Objective Marks : ");
+        System.out.println("Enter Objective Marks: ");
         objectiveMarks = scanner.nextFloat();
-        System.out.println("Enter Subjective Marks : ");
+        System.out.println("Enter Subjective Marks: ");
         subjectiveMarks = scanner.nextFloat();
-
     }
 
-    public void DisplayDetails(){
+    public void DisplayDetails() {
         super.DisplayDetails();
-        System.out.println("Objective Marks is : " + objectiveMarks);
-        System.out.println("Subjective Marks is : " + subjectiveMarks);
-
+        System.out.println("Objective Marks is: " + objectiveMarks);
+        System.out.println("Subjective Marks is: " + subjectiveMarks);
     }
-
 }
-class Sport extends Marks{
-    private float score;
-    public void AcceptDetails(){
+
+class Sport extends Marks {
+    protected float score;
+
+    public void AcceptDetails() {
         super.AcceptDetails();
-        System.out.println("Enter Sport Score : ");
+        System.out.println("Enter Sport Score: ");
         score = scanner.nextFloat();
-
-
     }
 
-    public void DisplayDetails(){
+    public void DisplayDetails() {
         super.DisplayDetails();
-        System.out.println("Sport Score : " + score);
-
-
+        System.out.println("Sport Score: " + score);
     }
 }
-public class ClassAndObject {
-    public static void main(String arg[]){
-        Sport obj = new Sport();
-        obj.AcceptDetails();
-        obj.DisplayDetails();
 
+class Result extends Sport {
+    private float TotalMarks, AverageMarks;
+
+    void Calculate() {
+        TotalMarks = objectiveMarks + subjectiveMarks + score;
+        AverageMarks = TotalMarks / 3;
+        System.out.println("Total Marks: " + TotalMarks);
+        System.out.println("Average Marks: " + AverageMarks);
+    }
+}
+
+public class ClassAndObject {
+    public static void main(String arg[]) {
+        Result result = new Result();
+        result.AcceptDetails();
+        result.DisplayDetails();
+        result.Calculate();
     }
 }
