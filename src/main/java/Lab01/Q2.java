70d4f4474552f614710305ee766b11e38e5f309d
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-01-12 18:29:37 +0530

    Lab q3

--- src/main/java/Lab01/Q2.java
+++ src/main/java/Lab01/Q2.java
@@ -4,24 +4,6 @@ import java.util.Scanner;
 
 public class Q2 {
     public static void main(String[] arg){
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Enter your full name:");
-//        String fullName = scanner.nextLine();
-//
-//        int firstSpaceIndex = fullName.indexOf("");
-//        int lastSpaceIndex = fullName.lastIndexOf("");
-//
-//        String firstName = fullName.substring(0,firstSpaceIndex);
-//        String middleName = fullName.substring(firstSpaceIndex+1,lastSpaceIndex);
-//        String lastName = fullName.substring(lastSpaceIndex+1);
-//
-//        String middleInitial = middleName.charAt(0) + "." ;
-//
-//        String formatName = lastName + "," + firstName + "" + middleInitial;
-//
-//        System.out.println("Format name: " + formatName);
-
-
         // Create a Scanner object to read input from the user.
         Scanner scanner = new Scanner(System.in);
         // Prompt the user to enter their full name.
